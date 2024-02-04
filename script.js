be6c7bd7c68a38fe2f3a991f5e4d6486aa0ead68
SeventhSG <158860005+SeventhSG@users.noreply.github.com>
2024-02-05 01:15:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function redirectToInstagram() {
-  window.location.href = "https://www.instagram.com/ozito77_";
-}
 
-function enlargeYesButton() {
-  let yesButton = document.getElementById('yesButton');
-  yesButton.style.transform = 'scale(1.2)';
-  redirectToInstagram();  // If you want to redirect to Instagram when "No" is clicked
-}
