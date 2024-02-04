2efb739cdcf69e2a4b740649ae979b186509418e
SeventhSG <158860005+SeventhSG@users.noreply.github.com>
2024-02-05 00:59:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,7 @@
-body {
-  font-family: 'Arial', sans-serif;
-  margin: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #f9f9f9;
+function redirectToInstagram() {
+  window.location.href = "https://www.instagram.com/ozito77_";
 }
 
-.container {
-  text-align: center;
-}
-
-img {
-  max-width: 100%;
-  height: auto;
-}
-
-h1 {
-  margin-top: 20px;
-}
-
-.buttons {
-  margin-top: 20px;
-}
-
-button {
-  padding: 10px 20px;
-  margin: 0 10px;
-  font-size: 16px;
-}
-
-#yesButton {
-  background-color: #4CAF50; /* Green */
-  color: white;
-}
-
-#noButton:hover {
-  cursor: pointer;
+function enlargeYesButton() {
+  document.getElementById('yesButton').classList.add('grow');
 }
